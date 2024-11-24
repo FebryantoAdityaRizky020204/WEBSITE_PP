876bab5f391edbf3dcb25c0160b3843cdcad5a0a
ADITYA_RIZKY_F <febryantoadityarizky@gmail.com>
2024-11-24 10:11:16 +0700

    Some [Change|Delete]: memindahkan direktori utama website, dan menghapus resource yang tidak digunakan

--- HimeeLaundry/routes/web.php
+++ HimeeLaundry/routes/web.php
@@ -1,9 +1,81 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\Auth\ForgotPassword;
+use App\Http\Livewire\Auth\Register;
+use App\Http\Livewire\Auth\ResetPassword;
+use App\Http\Livewire\Billing;
+use App\Http\Livewire\ExampleLaravel\UserManagement;
+use App\Http\Livewire\ExampleLaravel\UserProfile;
+use App\Http\Livewire\Notifications;
+use App\Http\Livewire\Profile;
+use App\Http\Livewire\StaticSignIn;
+use App\Http\Livewire\StaticSignUp;
+use App\Http\Livewire\Tables;
 
-Route::get('/', function () {
-    return view('welcome');
+use App\Http\Livewire\GuestLivewire;
+use App\Http\Livewire\CheckLivewire;
+
+use App\Http\Livewire\Auth\LoginLivewire;
+use App\Http\Livewire\JenisServiceLaundryLivewire;
+use App\Http\Livewire\DashboardLivewire;
+use App\Http\Livewire\TransactionsLivewire;
+use App\Http\Livewire\DetailTransactionsLivewire;
+use App\Http\Livewire\FinanceLivewire;
+use GuzzleHttp\Middleware;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('forgot-password', ForgotPassword::class)->middleware('guest')->name('password.forgot');
+Route::get('reset-password/{id}', ResetPassword::class)->middleware('signed')->name('reset-password');
+
+
+
+Route::get('sign-up', Register::class)->middleware('guest')->name('register');
+// Route::get('sign-in', Login::class)->middleware('guest')->name('login');
+
+Route::get('user-profile', UserProfile::class)->middleware('auth')->name('user-profile');
+Route::get('user-management', UserManagement::class)->middleware('auth')->name('user-management');
+
+
+// USED
+Route::get('/', function(){
+    return redirect(route('user-home'));
 });
 
+Route::group(['middleware' => 'guest'], function () {
+    Route::get('/', GuestLivewire::class)->name('user-home');
+    Route::prefix('user')->group(function () {
+        Route::get('/home', GuestLivewire::class)->name('user-home');
+        Route::get('/check', CheckLivewire::class)->name('user-check');
+    });
+});
 
+Route::get('admin/sign-in', LoginLivewire::class)->middleware('guest')->name('login');
+
+Route::group(['middleware' => 'auth'], function () {
+    Route::prefix('admin')->group(function () {
+        Route::get('billing', Billing::class)->name('billing');
+        Route::get('profile', Profile::class)->name('profile');
+        Route::get('tables', Tables::class)->name('tables');
+        Route::get('notifications', Notifications::class)->name("notifications");
+        
+        // USED
+        Route::get('/', DashboardLivewire::class)->name('dashboard');
+        Route::get('dashboard', DashboardLivewire::class)->name('dashboard');
+        Route::get('transactions', TransactionsLivewire::class)->name('transactions');
+        Route::get('transactions/{id}', DetailTransactionsLivewire::class)->name('detail-transactions');
+        Route::get('services', JenisServiceLaundryLivewire::class)->name('services');
+        Route::get('finance', FinanceLivewire::class)->name('finance');
+        Route::get('/check', GuestLivewire::class)->name('admin-check');
+    });
+});
